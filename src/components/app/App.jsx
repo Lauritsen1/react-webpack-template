262bcb443c43f2c77931086505c1c4e410bdaa38
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2021-04-10 17:02:04 +0200

    Removed useless stuff

---
+++ src/components/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/_global.scss'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    );
+}
+
+export default App;
